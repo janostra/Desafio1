eed01f43030c7ffb06a3b1e7322e9fe0281742c5
Jano <jano.stratakis123@gmail.com>
2024-02-27 18:52:52 -0300

    Desafio complementario

--- src/database.js
+++ src/database.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 
 //Conectar la base de datos
 
-mongoose.connect("mongodb+srv://janostra:4fZ2bd5ATBeIuUE1@cluster0.oyi4lr4.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://1@cluster0.oyi4lr4.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0")
     .then(()=>console.log("Conexion exitosa"))
     .catch(()=>console.log("Error en la conexion"))
+
+    //FALTA CONECTARSE CON EL SERVIDOR, SE QUITÓ USUARIO Y CONTRASEÑA
